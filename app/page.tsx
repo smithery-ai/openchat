5abe625c431754cfd321363f56cf499809a96f83
Anirudh Kamath <github@kamath.io>
2026-01-24 15:19:06 -0500

    proper suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
 "use server";
 import { Suspense } from "react";
 import { HomePage } from "@/components/home";
+import { Spinner } from "@/components/ui/spinner";
 import { getApiKey, getDefaultNamespace } from "@/lib/actions";
 
-export default async function Home() {
+async function HomePageWrapper() {
+	await new Promise((resolve) => setTimeout(resolve, 5_000));
 	const initialTokenResponse = await getApiKey();
 	const namespace = await getDefaultNamespace();
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<HomePage
-				initialTokenResponse={initialTokenResponse}
-				namespace={namespace}
-			/>
+		<HomePage
+			initialTokenResponse={initialTokenResponse}
+			namespace={namespace}
+		/>
+	);
+}
+
+export default async function Home() {
+	return (
+		<Suspense
+			fallback={
+				<div className="flex items-center justify-center h-screen gap-2">
+					<Spinner className="size-8" /> Fetching API Keys...
+				</div>
+			}
+		>
+			<HomePageWrapper />
 		</Suspense>
 	);
 }
